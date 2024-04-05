bc69e6cfc38ae5c11cdd28ec8b313cdbc7bcbcab
ArielDiazNaso <163585125+ArielDiazNaso@users.noreply.github.com>
2024-04-05 15:43:00 -0300

    Update TPN°6 FUNCIONES - string SECRETO.cpp
    Le saque el else

--- TPN°6 FUNCIONES - string SECRETO.cpp
+++ TPN°6 FUNCIONES - string SECRETO.cpp
@@ -3,23 +3,27 @@
 #include <algorithm>
 using namespace std;
 
-string invertirTexto(string str) {
-    int start = 0, end = 0;
-    for (int i = 0; i < str.size(); i++) {
-        if (str[i] == '(') {
+string invertirTexto(string frase) {
+    int start = 0;
+    int end = 0;
+    for (int i = 0; i < frase.size(); i++) {
+        if (frase[i] == '(') {
             start = i;
-        } else if (str[i] == ')') {
+        }
+        if (frase[i] == ')') {
             end = i;
-            string temp = str.substr(start + 1, end - start - 1);
+            
+            string temp = frase.substr(start + 1, end - start - 1);
+            
             reverse(temp.begin(), temp.end());
-            str.replace(start + 1, end - start - 1, temp);
+            frase.replace(start + 1, end - start - 1, temp);
         }
     }
-    return str;
+    return frase;
 }
 
 string armarFrase(string frase) {
-    string resultado = "";
+    string resultado;
     for (int i = 0; i < frase.size(); i++) {
         if (frase[i] != '(' && frase[i] != ')') {
             resultado += frase[i];
