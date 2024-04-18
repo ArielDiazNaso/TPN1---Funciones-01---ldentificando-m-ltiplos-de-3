baebbb44c8fbd518ad6b40fa8300f0e6e82ed1e4
ArielDiazNaso <163585125+ArielDiazNaso@users.noreply.github.com>
2024-04-18 14:21:50 -0300

    Add files via upload

---
+++ TPN°9 FUNCIONES - string repaso.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string invertirParte(string texto, int inicio, int final) {
+	
+    for (int i = inicio, j = final; i < j; ++i, --j) {
+        char temp = texto[i];
+        texto[i] = texto[j];
+        texto[j] = temp;
+    }
+    return texto;
+}
+
+int main() {
+    int inicio;
+	int final;
+    string texto;
+
+    cin >> inicio;
+	cin >> final;
+    cin.ignore();
+    getline(cin, texto);
+
+    string textoInvertido = invertirParte(texto, inicio, final);
+
+    cout << textoInvertido << endl;
+
+}
+
